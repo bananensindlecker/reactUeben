c1af27c2076d035ddb328b52ae05e52b5fd2df85
levio <levi.ottersberg@gmail.com>
2025-04-20 21:48:58 +0200

    e

---
+++ myElectronTestApp/src/electron/gettingServerInfo.ts
@@ -0,0 +1,34 @@
+import osUtiles from "os-utils"
+import fs from "fs";
+
+export function pollResources() {
+    setInterval(async()=>{
+        let cpuUsage:any = await getCpuUsage();
+        let ramUsage:any =  getRamUsage();
+        let totalStorage = getStorage().total;
+        let freeStorage = getStorage().unusage;
+        cpuUsage = cpuUsage.toFixed(2) as unknown as number;
+        ramUsage = ramUsage.toFixed(2) as unknown as number;
+        totalStorage = totalStorage.toFixed(2) as unknown as number;
+        freeStorage = freeStorage.toFixed(2) as unknown as number;
+        console.log({cpuUsage,ramUsage,totalStorage,freeStorage})
+    },500)
+}
+function getCpuUsage() {
+    return new Promise(resolve =>{
+        osUtiles.cpuUsage(resolve);
+    } );
+}
+function getRamUsage() {
+    return 1 - osUtiles.freememPercentage()
+}
+function getStorage() {
+    const stats = fs.statfsSync(process.platform === "win32"?"C:///":"/")
+    const total = stats.bsize *stats.blocks;
+    const free = stats.bsize * stats.bfree;
+
+    return {
+        total: Math.floor(total / 1_000_000_000),
+        unusage: free /total,
+    };
+}
